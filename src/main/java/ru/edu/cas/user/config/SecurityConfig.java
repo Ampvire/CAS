b7c2de1bce4061378e7a87280cf7e1fd1e8ea859
Kogay <nataly.sun89@mailru>
2022-03-16 17:11:38 +0300

    Реализация функционала Client

--- src/main/java/ru/edu/cas/user/config/SecurityConfig.java
+++ src/main/java/ru/edu/cas/user/config/SecurityConfig.java
@@ -1,33 +1,33 @@
-//package ru.edu.cas.user.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf()
-//                .ignoringAntMatchers("/admin/update")
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/admin/**").hasAnyRole("Admin")
-//                .antMatchers("/user/**").hasAnyRole("Meneger")
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .exceptionHandling().accessDeniedPage("/authenticate_failed.jsp");
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//}
+package ru.edu.cas.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .ignoringAntMatchers("/admin/update")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasAnyRole("Admin")
+                .antMatchers("/user/**").hasAnyRole("Meneger")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .exceptionHandling().accessDeniedPage("/authenticate_failed.jsp");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
