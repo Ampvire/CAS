8d736724ed7879b075a2abe6ae304d659f56d177
Kogay <nataly.sun89@mailru>
2022-03-27 12:21:42 +0300

    Добавила доработки Жени по отчетности клиента

--- src/main/java/ru/edu/cas/client/repo/ClientReportRepository.java
+++ src/main/java/ru/edu/cas/client/repo/ClientReportRepository.java
@@ -1,8 +1,9 @@
 package ru.edu.cas.client.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.edu.cas.client.dao.ClientReport;
 import ru.edu.cas.client.dao.Client;
+import ru.edu.cas.client.dao.ClientFinance;
+import ru.edu.cas.client.dao.ClientReport;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface ClientReportRepository extends JpaRepository<ClientReport, Inte
 
     List<ClientReport> findByClientId(Client clientId);
     List<ClientReport> findByClientIdAndDate(Client clientId, String date);
+    ClientReport findByClientIdAndFinanceId(Client clientId, ClientFinance financeId);
 }
